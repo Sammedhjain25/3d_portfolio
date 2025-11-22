206ebec062cd70a063139dcd2363c1bc2a38044b
V Sammedh Jain <sammedhjain25@gmail.com>
2025-11-22 15:28:12 +0530

    updated

---
+++ src/constants/index.js
@@ -0,0 +1,324 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Highway Accidents Detection Alarm System",
+    // description:
+    //   "Facilitates purchases from international websites like Amazon and eBay, allowing customers to shop from these sites and have products delivered domestically.",
+    subDescription: [
+      "The Highway Accidents Detection Alarm System (HADAS) is a conceptual project focused on improving highway safety.",
+      " It explores the use of real-time detection and automated alert mechanisms for accident response.",
+      " The project explored the theoretical framework and system design behind integrating sensors and automation for emergency response.",
+      " It demonstrates understanding of automation, safety engineering, and smart transport concepts.",
+      " Though presented theoretically, it reflects strong analytical and problem-solving skills.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/gemini_hadas.png",
+    tags: [
+      // {
+      //   id: 1,
+      //   name: "C#",
+      //   path: "/assets/logos/csharp.svg",
+      // },
+      // {
+      //   id: 2,
+      //   name: ".Net",
+      //   path: "/assets/logos/dotnet.svg",
+      // },
+      // {
+      //   id: 3,
+      //   name: "Ef Core",
+      //   path: "/assets/logos/efcore.png",
+      // },
+      // {
+      //   id: 4,
+      //   name: "TailwindCSS",
+      //   path: "/assets/logos/tailwindcss.svg",
+      // },
+    ],
+  },
+  {
+    id: 2,
+    title: "Real Estate Web Application",
+    // description:
+    //   "A secure authentication and authorization system using Auth0 for seamless user management.",
+    subDescription: [
+      "Developed a real estate web platform that allows users to post, search, and filter properties based on location, budget, and type. Implemented secure user authentication and designed dynamic property listings for a seamless experience. Focused on responsive design and intuitive navigation to ensure accessibility across devices. Integrated search and filter functionalities to simplify property discovery for users. Demonstrated strong understanding of full-stack development and database connectivity. This project highlights my ability to build functional, user-centric web applications from end to end.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/gemini_realestate.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Django",
+        path: "/assets/logos/django.svg",
+      },
+      {
+        id: 3,
+        name: "MySQL",
+        path: "/assets/logos/mysql.svg",
+      },
+      {
+        id: 4,
+        name: "ORMs",
+        path: "/assets/logos/orm.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Fashion Knowledge System (FKS)",
+    // description:
+    //   "A modern, interactive web application built with Blazor WebAssembly and .NET Core.",
+    subDescription: [
+      "Developed an AI-driven Fashion Knowledge System aimed at enhancing understanding of fashion trends and user preferences. Focused on building an intelligent recommendation framework that connects style insights with digital wardrobe data. Implemented data-driven modeling and classification techniques to analyze evolving fashion patterns effectively. Designed the system to provide personalized suggestions and contextual insights for improved decision-making. This project highlights my expertise in combining AI, design thinking, and data analysis to create innovative fashion solutions.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/gemini_fks.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "FastAPI",
+        path: "/assets/logos/fastapi.svg",
+      },
+      {
+        id: 3,
+        name: "PostgreSQL",
+        path: "/assets/logos/postgresql.svg",
+      },
+      {
+        id: 4,
+        name: "ORMs",
+        path: "/assets/logos/orm.svg",
+      },
+      // {
+      //   id: 5,
+      //   name: "Mui",
+      //    path: "/assets/logos/tailwindcss.svg",
+      // },
+    ],
+  },
+  {
+    id: 4,
+    title: "Recipe Manager App",
+    // description:
+    //   "A lightweight C++ game engine designed for 2D and 3D game development.",
+    subDescription: [
+      "Developed a fully offline mobile application for creating, storing, and managing recipes with an elegant and responsive UI. Focused on delivering a smooth and intuitive user experience through efficient local data handling. Implemented recipe categorization, editable details, and theme customization for personalization. Designed the app to function seamlessly without internet dependency, ensuring reliability. Emphasized clean architecture and optimized performance for consistent usability. This project showcases my ability to build polished, user-focused mobile applications from concept to completion.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/gemini_flutter.png",
+    tags: [
+      {
+        id: 1,
+        name: "Flutter",
+        path: "/assets/logos/flutter.svg",
+      },
+      {
+        id: 2,
+        name: "API",
+        path: "/assets/logos/api.svg",
+      },
+      // {
+      //   id: 3,
+      //   name: "Git",
+      //   path: "/assets/logos/git.svg",
+      // },
+      // {
+      //   id: 4,
+      //   name: "Microsoft",
+      //   path: "/assets/logos/microsoft.svg",
+      // },
+    ],
+  },
+  // {
+  //   id: 5,
+  //   title: "WordPress Custom Theme",
+  //   description:
+  //     "A fully customizable WordPress theme optimized for performance and SEO.",
+  //   subDescription: [
+  //     "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+  //     "Integrated Tailwind CSS for modern styling and UI enhancements.",
+  //     "Optimized SEO and page speed using Vite.js for fast builds.",
+  //     "Implemented custom widgets and plugin compatibility for extended functionality.",
+  //   ],
+  //   href: "",
+  //   logo: "",
+  //   image: "/assets/projects/wordpress-theme.jpg",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "WordPress",
+  //       path: "/assets/logos/wordpress.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "HTML5",
+  //       path: "/assets/logos/html5.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "CSS3",
+  //       path: "/assets/logos/css3.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "Vite.js",
+  //       path: "/assets/logos/vitejs.svg",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 6,
+  //   title: "Online Learning Platform",
+  //   description:
+  //     "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+  //   subDescription: [
+  //     "Built using Blazor WebAssembly for a seamless SPA experience.",
+  //     "Implemented video streaming with Azure Media Services.",
+  //     "Added a quiz system with dynamic question generation and real-time grading.",
+  //     "Integrated Stripe API for secure payment processing.",
+  //   ],
+  //   href: "",
+  //   logo: "",
+  //   image: "/assets/projects/elearning.jpg",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "Blazor",
+  //       path: "/assets/logos/blazor.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "Azure",
+  //       path: "/assets/logos/azure.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "Stripe",
+  //       path: "/assets/logos/stripe.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "TailwindCSS",
+  //       path: "/assets/logos/tailwindcss.svg",
+  //     },
+  //   ],
+  // },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "https://wa.me/9483815608",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/sammedh-jain-589b22267?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/sammedh_jn_?igsh=MWl1NndydHVqNmFzOQ==",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "CPDM, IISc Bangalore",
+    job: "Oct–Dec 2025",
+    date: "Product Design Intern",
+    contents: [
+      "Contributing to the design and development of an intelligent product ecosystem integrating Flutter, React, FastAPI, and LLM-based modules.",
+      "Responsible for architecting scalable full-stack solutions, optimizing user interaction workflows, and implementing data-driven design iterations.",
+      "Focused on bridging product design research with AI-enhanced software development, emphasizing usability, performance, and innovation.",
+
+
+    ],
+  },
+  {
+    title: "Nexus Info Pvt Ltd",
+    job: "Sep–Oct 2024",
+    date: "Web Developer Intern ",
+    contents: [
+      "Developed a fully functional restaurant website with secure login and signup authentication.",
+      "Integrated front-end and backend systems to ensure smooth user registration and login workflows.",
+      "Focused on creating a responsive, intuitive, and visually appealing interface for customers.",
+      "Enhanced user experience through interactive design elements, optimized navigation, and consistent UI patterns.",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2025-Present",
+    contents: [
+      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
